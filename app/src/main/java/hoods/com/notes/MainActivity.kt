6702cf4665c597705f395e5e4053936b6f9f279b
PRS10 <87570813+PRS10@users.noreply.github.com>
2024-11-25 18:41:46 +0000

    Ultimos updates

--- app/src/main/java/hoods/com/notes/MainActivity.kt
+++ app/src/main/java/hoods/com/notes/MainActivity.kt
@@ -10,17 +10,29 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import hoods.com.notes.detail.DetailViewModel
+import hoods.com.notes.home.HomeViewModel
+import hoods.com.notes.login.LoginViewModel
 import hoods.com.notes.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val loginViewModel = viewModel(modelClass = LoginViewModel::class.java)
+            val homeViewModel = viewModel(modelClass = HomeViewModel::class.java)
+            val detailViewModel = viewModel(modelClass = DetailViewModel::class.java)
+
             NotesTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-
+                    Navigation(
+                        loginViewModel = loginViewModel,
+                        detailViewModel = detailViewModel,
+                        homeViewModel = homeViewModel
+                    )
                 }
             }
         }
